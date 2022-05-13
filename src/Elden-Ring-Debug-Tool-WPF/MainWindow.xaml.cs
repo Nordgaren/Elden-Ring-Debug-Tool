b196805f0d12041f9ca8d87017e8b0c4e7990505
nordgaren <nordgarentv@gmail.com>
2022-05-13 02:31:23 -0700

    Fixed window transition from max to min

--- src/Elden-Ring-Debug-Tool-WPF/MainWindow.xaml.cs
+++ src/Elden-Ring-Debug-Tool-WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Input;
 using AvalonDock.Themes;
 using Elden_Ring_Debug_Tool_WPF.Windows;
@@ -50,7 +51,42 @@ namespace Elden_Ring_Debug_Tool_WPF
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    WindowState = WindowState.Normal;
+                    double percentHorizontal = e.GetPosition(this).X / ActualWidth;
+                    double targetHorizontal = RestoreBounds.Width * percentHorizontal;
+
+                    double percentVertical = e.GetPosition(this).Y / ActualHeight;
+                    double targetVertical = RestoreBounds.Height * percentVertical;
+
+                    POINT lMousePosition;
+                    GetCursorPos(out lMousePosition);
+                    Left = lMousePosition.X - targetHorizontal;
+                    Top = lMousePosition.Y - targetVertical;
+
+                }
                 DragMove();
+            }
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
         }
 
         private void Minimize_Click(object sender, RoutedEventArgs e)
