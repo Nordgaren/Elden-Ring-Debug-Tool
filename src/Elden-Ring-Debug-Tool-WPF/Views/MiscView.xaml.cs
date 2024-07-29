9e1fd0c3f6946e81b23b55a089fc90ce20e30844
MercyMoon <45549722+SamDamDing@users.noreply.github.com>
2024-07-29 15:26:50 -0400

    Add files via upload

--- src/Elden-Ring-Debug-Tool-WPF/Views/MiscView.xaml.cs
+++ src/Elden-Ring-Debug-Tool-WPF/Views/MiscView.xaml.cs
@@ -37,228 +37,71 @@ namespace Elden_Ring_Debug_Tool_WPF.Views
                 _itemGibViewModel = vm;
             }
         }
+
+        private void CheckAllEventFlags(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    var eventFlags = File.ReadAllLines(filePath);
+                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
+                    resultsListBox?.Items.Clear();
+
+                    foreach (var line in eventFlags)
+                    {
+                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
+                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
+                        {
+                            _itemGibViewModel.EventFlag = eventFlag;
+                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
+                            resultsListBox?.Items.Add($"{_itemGibViewModel.IsEventFlag}: {parts[0]} - {parts[1]}");
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Invalid line format or event flag: {line}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show($"{System.IO.Path.GetFileName(filePath)} not found in the Resources folder.");
+            }
+        }
+
         private void CheckAllMimicTears_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = @"Resources\MimicTearsEventFlags.txt";
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    var eventFlags = File.ReadAllLines(filePath);
-                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
-                    if (resultsListBox != null) resultsListBox.Items.Clear();
-
-                    foreach (var line in eventFlags)
-                    {
-                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
-                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
-                        {
-                            _itemGibViewModel.EventFlag = eventFlag;
-                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
-                            if (resultsListBox != null)
-                                resultsListBox.Items.Add($"{parts[0]} - {parts[1]}: {_itemGibViewModel.IsEventFlag}");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Invalid line format or event flag: {line}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("EventFlags.txt not found in the Resources folder.");
-            }
+            CheckAllEventFlags(@"Resources\MimicTearsEventFlags.txt");
         }
+
         private void CheckAllLostAshes_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = @"Resources\LostAshesEventFlags.txt";
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    var eventFlags = File.ReadAllLines(filePath);
-                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
-                    if (resultsListBox != null) resultsListBox.Items.Clear();
-
-                    foreach (var line in eventFlags)
-                    {
-                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
-                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
-                        {
-                            _itemGibViewModel.EventFlag = eventFlag;
-                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
-                            if (resultsListBox != null)
-                                resultsListBox.Items.Add($"{parts[0]} - {parts[1]}: {_itemGibViewModel.IsEventFlag}");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Invalid line format or event flag: {line}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("EventFlags.txt not found in the Resources folder.");
-            }
+            CheckAllEventFlags(@"Resources\LostAshesEventFlags.txt");
         }
+
         private void CheckAllAncientStones_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = @"Resources\LostAncientStonesEventFlags.txt";
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    var eventFlags = File.ReadAllLines(filePath);
-                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
-                    if (resultsListBox != null) resultsListBox.Items.Clear();
-
-                    foreach (var line in eventFlags)
-                    {
-                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
-                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
-                        {
-                            _itemGibViewModel.EventFlag = eventFlag;
-                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
-                            if (resultsListBox != null)
-                                resultsListBox.Items.Add($"{parts[0]} - {parts[1]}: {_itemGibViewModel.IsEventFlag}");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Invalid line format or event flag: {line}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("EventFlags.txt not found in the Resources folder.");
-            }
+            CheckAllEventFlags(@"Resources\LostAncientStonesEventFlags.txt");
         }
+
         private void CheckAllSomberAncientStones_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = @"Resources\LostAncientSomberStonesEventFlags.txt";
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    var eventFlags = File.ReadAllLines(filePath);
-                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
-                    if (resultsListBox != null) resultsListBox.Items.Clear();
-
-                    foreach (var line in eventFlags)
-                    {
-                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
-                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
-                        {
-                            _itemGibViewModel.EventFlag = eventFlag;
-                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
-                            if (resultsListBox != null)
-                                resultsListBox.Items.Add($"{parts[0]} - {parts[1]}: {_itemGibViewModel.IsEventFlag}");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Invalid line format or event flag: {line}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("EventFlags.txt not found in the Resources folder.");
-            }
+            CheckAllEventFlags(@"Resources\LostAncientSomberStonesEventFlags.txt");
         }
+
         private void CheckAllGreatGraveGloves_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = @"Resources\LostGreatGraveGlovewortEventFlags.txt";
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    var eventFlags = File.ReadAllLines(filePath);
-                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
-                    if (resultsListBox != null) resultsListBox.Items.Clear();
-
-                    foreach (var line in eventFlags)
-                    {
-                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
-                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
-                        {
-                            _itemGibViewModel.EventFlag = eventFlag;
-                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
-                            if (resultsListBox != null)
-                                resultsListBox.Items.Add($"{parts[0]} - {parts[1]}: {_itemGibViewModel.IsEventFlag}");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Invalid line format or event flag: {line}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("EventFlags.txt not found in the Resources folder.");
-            }
+            CheckAllEventFlags(@"Resources\LostGreatGraveGlovewortEventFlags.txt");
         }
+
         private void CheckAllGreatGhostGloves_Click(object sender, RoutedEventArgs e)
         {
-            string filePath = @"Resources\LostGreatGhostGlovewortEventFlags.txt";
-            if (File.Exists(filePath))
-            {
-                try
-                {
-                    var eventFlags = File.ReadAllLines(filePath);
-                    var resultsListBox = FindName("EventFlagResultsListBox") as ListBox;
-                    if (resultsListBox != null) resultsListBox.Items.Clear();
-
-                    foreach (var line in eventFlags)
-                    {
-                        var parts = line.Split(new[] { ' ' }, 2);  // Split the line into two parts at the first space
-                        if (parts.Length == 2 && int.TryParse(parts[0], out int eventFlag))
-                        {
-                            _itemGibViewModel.EventFlag = eventFlag;
-                            (_itemGibViewModel.CheckEventFlag as IsEventCommand)?.Execute(null);
-                            if (resultsListBox != null)
-                                resultsListBox.Items.Add($"{parts[0]} - {parts[1]}: {_itemGibViewModel.IsEventFlag}");
-                        }
-                        else
-                        {
-                            MessageBox.Show($"Invalid line format or event flag: {line}");
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"An error occurred: {ex.Message}");
-                }
-            }
-            else
-            {
-                MessageBox.Show("EventFlags.txt not found in the Resources folder.");
-            }
+            CheckAllEventFlags(@"Resources\LostGreatGhostGlovewortEventFlags.txt");
         }
-
     }
 }
